34ffba1aa8d5d36b55dc9d81a1627e28881c3897
Arthur PIGEON <apigeon@student.42.fr>
2022-11-03 19:06:32 +0100

    created struct for parsing and fixed isspace function in exit

--- incl/parser.h
+++ incl/parser.h
@@ -6,7 +6,7 @@
 /*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 13:59:39 by apigeon           #+#    #+#             */
-/*   Updated: 2022/11/02 10:31:31 by apigeon          ###   ########.fr       */
+/*   Updated: 2022/11/03 11:49:40 by apigeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,34 @@ typedef enum e_token_type
 	TOKEN_WORD,
 }			t_token_type;
 
+typedef enum e_redirect_type
+{
+	RD_IN,
+	RD_OUT,
+	RD_APPEND,
+	RD_HEREDOC,
+}			t_redirect_type;
+
 typedef struct s_token
 {
 	t_token_type	type;
 	char			*value;
 }				t_token;
+
+typedef struct s_redirect
+{
+	int				fd;
+	char			*file;
+	t_redirect_type	type;
+}				t_redirect;
+
+typedef struct s_command
+{
+	char	*cmd_full;
+	char	*cmd;
+	char	**args;
+	t_list	*redirects;
+}				t_command;
 
 void	*parse_line(char *line);
 t_list	*get_tokens(char *line);
--- src/parser/parse.c
+++ src/parser/parse.c
@@ -6,21 +6,41 @@
 /*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 14:50:12 by apigeon           #+#    #+#             */
-/*   Updated: 2022/11/02 09:29:00 by apigeon          ###   ########.fr       */
+/*   Updated: 2022/11/03 11:17:34 by apigeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
+static char	*get_token_name(t_token_type type)
+{
+	if (type == TOKEN_PIPE)
+		return (ft_strdup("PIPE\t"));
+	else if (type == TOKEN_RD_IN)
+		return (ft_strdup("RD_IN\t"));
+	else if (type == TOKEN_RD_OUT)
+		return (ft_strdup("RD_OUT\t"));
+	else if (type == TOKEN_RD_APPEND)
+		return (ft_strdup("RD_APPEND"));
+	else if (type == TOKEN_RD_HEREDOC)
+		return (ft_strdup("RD_HEREDOC"));
+	else if (type == TOKEN_WORD)
+		return (ft_strdup("WORD\t"));
+	return (ft_strdup("NONE\t"));
+}
+
 static void	print_tokens(t_list *tokens)
 {
+	char	*type;
 	t_token	*token;
 
 	while (tokens)
 	{
 		token = tokens->content;
-		printf("Token: value->%s, type->%d\n", token->value, token->type);
+		type = get_token_name(token->type);
+		printf("Token: %s->\t%s\n", type, token->value);
 		tokens = tokens->next;
+		free(type);
 	}
 }
 
