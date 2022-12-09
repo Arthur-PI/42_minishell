f249f04e967f3392f462042791bcba324f4d6482
tea <tea@Ubuntu2.tea.virtualbox.org>
2022-12-09 14:33:33 +0100

    norme

--- incl/builtins.h
+++ incl/builtins.h
@@ -6,7 +6,7 @@
 /*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 17:51:12 by apigeon           #+#    #+#             */
-/*   Updated: 2022/11/30 14:38:59 by tperes           ###   ########.fr       */
+/*   Updated: 2022/12/09 13:24:47 by tperes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,17 @@ typedef struct s_env
 	char	*value;
 }				t_env;
 
-void		free_env(void *ptr);
 int		pwd(int ac);
 int		echo(int ac, char **av);
 int		my_exit(int ac, char **av);
 int		cd(int ac, char **av);
-t_list		*tab_to_list(char **env);
-t_env		*create_env(char *env);
-int		my_env(int ac, char **av, char **env);
-int		my_unset(int ac, char **av, char **env);
-int		my_export(int ac, char **av, char **env);
-t_list		*add_env(t_list *lst, char *env);
+int		my_env(int ac, char **av);
+int		my_unset(int ac, char **av);
+int		my_export(int ac, char **av);
+void	free_env(void *ptr);
+t_list	*add_env(t_list *lst, char *env);
+t_env	*get_env_el(char *s);
+t_list	*tab_to_list(char **env);
+t_env	*create_env(char *env);
 
 #endif
