d99375b2a1e6e979796b27d2ffad6d4467bbeecd
tea <tea@Ubuntu2.tea.virtualbox.org>
2022-12-12 23:50:29 +0100

    cd sans argument

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,12 +6,14 @@
 /*   By: tperes <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 16:47:15 by tperes            #+#    #+#             */
-/*   Updated: 2022/11/01 17:16:29 by tperes           ###   ########.fr       */
+/*   Updated: 2022/12/12 23:07:52 by tperes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
+#include "minishell.h"
 
+extern t_minishell	g_minishell;
 /*
 - cd with a directory that exists
 - cd with a directory that doesn't exist
@@ -19,11 +21,34 @@
 TODO cd with no argument
 */
 
+char	*home(void)
+{
+	t_env	*env;
+	t_list	*lst;
+	char	*home;
+
+	lst = g_minishell.envs;
+	while (lst != NULL)
+	{
+		env = lst->content;
+		if (env == get_env_el("HOME"))
+			home = env->value;
+		lst = lst->next;
+	}
+	return (home);
+}
+
 int	cd(int ac, char **av)
 {
-	if (ac > 2)
+
+	if (ac == 1)
+		chdir(home());
+	else if (ac > 2)
 		return (printf("cd: string not in pwd: %s\n", av[1]), 1);
-	if (chdir(av[1]) == -1)
-		return (printf("cd: no such file or directory: %s\n", av[1]), 1);
+	else if (ac == 2)
+	{
+		if (chdir(av[1]) == -1)
+			return (printf("cd: no such file or directory: %s\n", av[1]), 1);
+	}
 	return (0);
 }
