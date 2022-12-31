ce655ddf369fb2788a196dcd5fe08c578794d19d
tea <tea@Ubuntu2.tea.virtualbox.org>
2022-12-31 09:15:21 +0100

    norm

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,19 +6,21 @@
 /*   By: tperes <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 16:47:15 by tperes            #+#    #+#             */
-/*   Updated: 2022/12/24 07:24:42 by tperes           ###   ########.fr       */
+/*   Updated: 2022/12/31 09:12:40 by tperes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
 extern t_minishell	g_minishell;
+
 /*
 - cd with a directory that exists
 - cd with a directory that doesn't exist
 - cd with many arguments
 TODO cd with no argument
 */
+
 char	*home(void)
 {
 	t_env	*env;
@@ -40,7 +42,6 @@ char	*home(void)
 // TODO FIX handle chdir return value in case of error
 int	cd(int ac, char **av)
 {
-
 	if (ac == 1)
 	{
 		if (chdir(home()) == -1)
@@ -51,7 +52,10 @@ int	cd(int ac, char **av)
 	else if (ac == 2)
 	{
 		if (chdir(av[1]) == -1)
-			return (printf("cd: no such file or directory: %s\n", av[1]), 1);
+		{
+			printf("cd: no such file or directory: %s\n", av[1]);
+			return (1);
+		}
 	}
 	return (0);
 }
