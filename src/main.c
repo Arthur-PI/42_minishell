014830a563fe5d7e7916fc0a073652c2bbc4b41d
Tea PERES <tperes@e3r9p13.clusters.42paris.fr>
2023-01-09 13:26:34 +0100

    invalid free, lstclear en commentaire

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: apigeon <apigeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 18:20:54 by apigeon           #+#    #+#             */
-/*   Updated: 2022/12/11 13:55:34 by apigeon          ###   ########.fr       */
+/*   Updated: 2023/01/09 12:02:33 by tperes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ static void	routine(void)
 		if (!commands)
 			break ;
 		executing(commands);
-		ft_lstclear(&commands, &free_command);
+	//	ft_lstclear(&commands, &free_command);
 	}
 	ft_lstclear(&g_minishell.envs, free_env);
 }
