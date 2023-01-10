26d553f58c5bb7548b859f96c41a9e8bdab4c4c8
tea <tea@Ubuntu2.tea.virtualbox.org>
2023-01-10 14:29:05 +0100

    append

--- src/execution/redir.c
+++ src/execution/redir.c
@@ -6,7 +6,7 @@
 /*   By: tperes <tperes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 16:29:02 by tperes            #+#    #+#             */
-/*   Updated: 2023/01/09 14:50:04 by tperes           ###   ########.fr       */
+/*   Updated: 2023/01/10 14:18:32 by tperes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,9 @@ int	redir_input(int tpin, t_list *command)
 		while (tmp_rd != NULL)
 		{
 			redirect = tmp_rd->content;
-			if (redirect->type == RD_IN)
-			{
-				printf("%s\n", redirect->file);
+			if (redirect->type == RD_IN
+				|| redirect->type == RD_HEREDOC)
 				fdin = dup(redirect->fd);
-			}
 			tmp_rd = tmp_rd->next;
 		}
 		tmp_command = tmp_command->next;
@@ -56,6 +54,9 @@ int	redir_output(int tpout, t_list *command)
 			redirect = cmd->redirects->content;
 			if (redirect->type == RD_OUT)
 				fdout = open(redirect->file, O_WRONLY);
+			if (redirect->type == RD_APPEND)
+				fdout = open(redirect->file, O_APPEND
+						| O_WRONLY);
 			cmd->redirects = cmd->redirects->next;
 		}
 		command = command->next;
