7bc19bd9efbd47e83bd153ef8a759e8c8ea5b7dd
Tea PERES <tperes@e1r5p6.clusters.42paris.fr>
2023-01-22 18:32:16 +0100

    fix: LONG_MAX, LONG_MIN, plusieurs signes, verif_number

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: tperes <tperes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 16:09:07 by tperes            #+#    #+#             */
-/*   Updated: 2023/01/21 14:49:00 by tperes           ###   ########.fr       */
+/*   Updated: 2023/01/22 18:22:07 by tperes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 extern t_minishell	g_minishell;
 
-static long long	atoi_exit(const char *str)
+static long	atoi_exit(const char *str)
 {
 	int			i;
 	int			sign;
@@ -48,22 +48,52 @@ static void	free_exit(int status)
 	exit(status);
 }
 
+static bool	is_valid(char **av)
+{
+	int	sign;
+
+	sign = (av[1][0] == '-' || av[1][0] == '+');
+	if ((size_t)(19 + sign) < ft_strlen(av[1]))
+		return (false);
+	if (av[1][0] != '-' && ft_strcmp(av[1] + sign, "9223372036854775808") >= 0)
+		return (false);
+	if (av[1][0] == '-' && ft_strcmp(av[1] + 1, "9223372036854775808") > 0)
+		return (false);
+	return (true);
+}
+
+static bool	verif_number(char **av)
+{
+	int	i;
+
+	i = 0;
+	if (av[1][i] == '-' || av[1][i] == '+')
+		i++;
+	while (av[1][i])
+	{
+		if (ft_isdigit(av[1][i]) == 0)
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 int	my_exit(int ac, char **av)
 {
-	int	status;
+	int		status;
+	long	value;
 
 	printf("exit\n");
 	status = g_minishell.exit_status;
 	if (ac > 1)
 	{
-		if ((av[1][0] == '-' && ft_strcmp(av[1], "-9223372036854775809") >= 0)
-			|| ft_strcmp(av[1], "9223372036854775808") >= 0
-			|| (atoi_exit(av[1]) == 0 && av[1][0] != '0'))
+		value = atoi_exit(av[1]);
+		if (!verif_number(av) || !is_valid(av))
 		{
 			printf("minishell: exit: %s: numeric argument required\n", av[1]);
 			free_exit(2);
 		}
-		status = atoi_exit(av[1]);
+		status = value;
 	}
 	if (ac > 2)
 	{
